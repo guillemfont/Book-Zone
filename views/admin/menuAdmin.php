4693d9a98ea8a34361ff2ac8265869ff83c4ee5a
Onetti01 <albertonetti@gmail.com>
2022-11-11 10:56:00 +0100

    menuadmin

--- views/admin/menuAdmin.php
+++ views/admin/menuAdmin.php
@@ -1,15 +1,16 @@
-<?php
-echo'<a>Hola soy admin
-<a href="index.php?controller=Admin&action=closeAdmin">Cerrar Sesion</a>
-<table>
-    <tr>
-        <th>id</th>
-        <th>nombre</th>
-        <th>descripcion</th>
-        <th>precio</th>
-        <th>foto</th>
-        <th>stock</th>
-        <th>id_categoria</th>
-    </tr>
-</table>';
-?>
+<div>
+    Hola soy admin
+    <a href="index.php?controller=Admin&action=closeAdmin">Cerrar Sesion</a>
+</div>
+</div>
+
+
+<!-- <header>
+    <nav>
+        <a href="#"><i class="fa-sharp fa-solid fa-house"></i></a>
+        <a href="#">Categories</a>
+        <a href="#">Productes</a>
+        <a href="#">Registrar-se</a>
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+    </nav>
+</header> -->
