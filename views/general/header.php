fe0937d66cba361f3a446045958a44ce324451a8
guillemfont <guillem.font@inslapineda.cat>
2022-11-25 18:47:52 +0100

    ajustes cabecera

--- views/general/header.php
+++ views/general/header.php
@@ -1,46 +1,60 @@
-<header class="menu">
+<?php 
+
+echo '<header class="menu">
     <nav class="menuContainer">
         <h1 class="menuLogo"><img src="assets/img/img_general/logo.png" alt="" srcset=""></h1>
-
+        
         <ul class="menuLinks">
-        <li class="menuItem">
-            <a href="#" class="menuLink"><i class="fa-solid fa-house"></i></a>
-        </li>
-        <li class="menuItem menuItemShow">
-            <a href="#" class="menuLink"><p>About</p><i class="fa-solid fa-angle-up" id="menuArrow"></i></a>
-            <ul class="menuNesting">
+            <li class="menuItem">
+                <a href="index.php" class="menuLink"><i class="fa-solid fa-house"></i></a>
+            </li>
+            <li class="menuItem menuItemShow">
+                <a href="#" class="menuLink"><p>Categorías</p><i class="fa-solid fa-angle-up" id="menuArrow"></i></a>
+                <ul class="menuNesting">
                 <li class="menuInside">
-                    <a href="#" class="menuLinkInside">About 1</a>
+                    <a href="#" class="menuLinkInside">Novela</a>
                 </li>
                 <li class="menuInside">
-                    <a href="#" class="menuLinkInside">About 2</a>
+                    <a href="#" class="menuLinkInside">Ficción</a>
                 </li>
                 <li class="menuInside">
-                    <a href="#" class="menuLinkInside">About 3</a>
-                </li>
-            </ul>
-        </li>
-        <li class="menuItem menuItemShow">
-        <a href="#" class="menuLink"><p>Project</p><i class="fa-solid fa-angle-up" id="menuArrow"></i></a>
-            <ul class="menuNesting">
-                <li class="menuInside">
-                    <a href="#" class="menuLinkInside">Project 1</a>
+                    <a href="#" class="menuLinkInside">Comedia</a>
                 </li>
                 <li class="menuInside">
-                    <a href="#" class="menuLinkInside">Project 2</a>
+                    <a href="#" class="menuLinkInside">Terror</a>
                 </li>
                 <li class="menuInside">
-                    <a href="#" class="menuLinkInside">Project 3</a>
+                    <a href="#" class="menuLinkInside">Fantasía</a>
                 </li>
             </ul>
         </li>
         <li class="menuItem">
-            <a href="#" class="menuLink"><i class="fa-solid fa-house"></i></a>
+                <a href="index.php" class="menuLink"><i class="fa-solid fa-cart-shopping"></i></a>
+            </li>
+        ';
+    if (!isset($_SESSION['client'])){
+
+     echo '   <li class="menuItem">
+        <a href="index.php?log=true&controller=Client&action=loginClient" class="menuLink"><p>Acceder</p></i></a>
         </li>
-    </ul>
+        <li class="menuItem">
+        <a href="index.php?controller=Client&action=registerClient" class="menuLink"><div class="menuLinkContainer"><p>Registrarse</p></div></a>';
+    } else {
+        echo '   <li class="menuItem">
+        <a href="index.php?log=true&controller=Client&action=loginClient" class="menuLink"><p>Acceder</p></i></a>
+        </li>
+        <li class="menuItem">
+        <a href="index.php?controller=Client&action=registerClient" class="menuLink"><div class="menuLinkContainer"><p>Registrarse</p></div></a>';
+    }
 
-    <menu class="menuDisplay"><i class="fa-solid fa-bars" id="menuBars"></i></menu>
 
-    </nav>
-
-</header>
+    echo '
+            </li>
+            </ul>
+            
+            <menu class="menuDisplay"><i class="fa-solid fa-bars" id="menuBars"></i></menu>
+            
+            </nav>
+            
+            </header>';
+?>
